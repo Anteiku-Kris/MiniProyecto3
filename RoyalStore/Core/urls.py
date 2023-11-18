f2d4eb847cd02976efc197f61b165914aa16569e
Kristopher Castillo Gómez <akriscastillo@gmail.com>
2023-11-18 13:54:55 -0600

    Se termino el registro de usuario, cuando se registra un usuario exitosamente podemos ver un mensaje de success

--- RoyalStore/Core/urls.py
+++ RoyalStore/Core/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 app_name = "Core"
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('products/', views.products, name="products"),
+    path('registro/', views.registro, name='registro'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
--- RoyalStore/Core/views.py
+++ RoyalStore/Core/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login
 
 def home(request):
     return render(request, "home.html")
 
 def products(request):
     return render(request, "products.html")
+
+def registro(request):
+    data={
+        'form': CustomUserCreationForm()
+    }
+    if request.method == "POST":
+        formulario = CustomUserCreationForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            user = authenticate(username=formulario.cleaned_data["username"], password=formulario.cleaned_data["password1"])
+            login(request, user)
+            messages.success(request, "Te has registrado correctamente")
+            return redirect(to="Core:home")
+        data["form"] = formulario
+    return render(request, "registration/registro.html", data)
