7095bdf72be394a55a6bbe9b7b64c69a2bed66a0
ManuelAlrod <manuelalmengor32@gmail.com>
2023-12-09 18:16:31 -0500

    Se arreglo las plantillas de html y se agrego algunos css

--- RoyalStore/Core/models.py
+++ RoyalStore/Core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Productos(models.Model):
+    class Meta:
+        verbose_name_plural = 'Productos'
+        verbose_name = 'Producto'
+        ordering = ["name"]
+
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(blank=True, null=True, upload_to="productos")
+    category = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Resena(models.Model):
+    producto = models.ForeignKey(Productos, on_delete=models.CASCADE)
+    comentario = models.TextField()
+    puntuacion = models.PositiveIntegerField(default=0)  # Campo para almacenar la puntuación
+
+    def __str__(self):
+        return f'Reseña de {self.producto.name}'
--- RoyalStore/Core/urls.py
+++ RoyalStore/Core/urls.py
@@ -6,7 +6,11 @@ app_name = "Core"
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('products/', views.products, name="products"),
+    path('products/', views.productosvistas, name="productos"),
+    path('agregar-productos/', views.agregarproducto, name="agregar-productos"),
+    path('editar-productos/', views.editarproducto, name='editar-productos'),
+    path('eliminar-productos/', views.eliminarproducto, name='eliminar-productos'),
+    path('producto/<int:producto_id>/', views.detallesproductos, name='producto'),
     path('registro/', views.registro, name='registro'),
     path('contacts/', views.contacts, name="contacts"),
     path('login/', LoginView.as_view(), name='login'),
